2a842a72c8ed41a1ce923dad51598e17b4cc9c55
Mark Chouinard <mark@chouinard.me>
2024-07-08 15:10:06 -0700

    edit notes

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,14 +1,16 @@
 import type { ActionFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
 import type { Note } from "~/models/note.server";
 import { deleteNote, getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 import invariant from "tiny-invariant";
+import { useRef } from "react";
 
 type LoaderData = {
   note: Note;
 };
+
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const userId = await requireUserId(request);
@@ -23,30 +25,56 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 }
 
 export const action: ActionFunction = async ({ request, params }) => {
+  console.log('params', params);
   const userId = await requireUserId(request);
   invariant(params.noteId, "noteId not found");
 
-  await deleteNote({ userId, id: params.noteId });
+  const formData = await request.formData();
+  const buttonAction = formData.get("buttonAction") as string;
 
-  return redirect("/notes");
+  console.log('buttonAction', buttonAction);
+
+  switch (buttonAction) {
+    case "delete":
+      await deleteNote({ userId, id: params.noteId });
+      return redirect("/notes");
+    case "edit":
+      return redirect(`/notes/edit/${params.noteId}`);
+    default:
+      throw new Error("Invalid buttonAction");
+  }
+
+
 };
 
 export default function NoteDetailsPage() {
   const data = useLoaderData<typeof loader>() as LoaderData;
+  const fetcher = useFetcher();
+  const formRef = useRef<HTMLFormElement>(null);
 
   return (
     <div>
       <h3 className="text-2xl font-bold">{data.note.title}</h3>
       <p className="py-6">{data.note.body}</p>
       <hr className="my-4" />
-      <Form method="post">
+      <fetcher.Form ref={formRef} method="post">
         <button
           type="submit"
-          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          className="rounded bg-red-500  py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400"
+          name="buttonAction"
+          value="delete"
         >
           Delete
         </button>
-      </Form>
+        <button
+          type="submit"
+          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          name="buttonAction"
+          value="edit"
+        >
+          Edit
+        </button>
+      </fetcher.Form>
     </div>
   );
 }
